786c1847fd64c0372c59ff3bee2e77a18d52d263
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2024-06-14 23:22:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,36 +2,44 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import { readdirSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { fileURLToPath } from 'url'; // Import fileURLToPath function from url module
+import { dirname, join } from 'path'; // Import dirname and join functions from path module
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); // Convert the URL to a file path
+const __dirname = dirname(__filename); // Get the directory name
 
 const app = express();
 
 // Database
 mongoose
-  .connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.DATABASE)
   .then(() => console.log('Database connected'))
   .catch((err) => console.log('DB connection error =>', err));
 
 // Middlewares
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+const corsConfig = {
+  origin: "*",
+  credential:true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}
+app.options("",cors(corsConfig));
+app.use(cors(corsConfig));
 
 // Load Routes dynamically
-const routePath = join(__dirname, 'routes');
+const routePath = join(__dirname, 'routes'); // Get the path to the routes directory
 const routeFiles = readdirSync(routePath);
 
-routeFiles.forEach((file) => {
-  import(`file://${join(routePath, file)}`).then(({ default: route }) => {
-    app.use('/api', route);
-  }).catch((err) => console.error(`Error loading route ${file}:`, err));
+routeFiles.forEach(async (file) => {
+  const { default: route } = await import(`file://${join(routePath, file)}`); // Import the route using file:// URL scheme
+  app.use('/api', route); // Mount the route
 });
 
 app.use((err, req, res, next) => {
@@ -42,12 +50,10 @@ app.use((err, req, res, next) => {
   next();
 });
 
-// Default route
-app.get("/", (req, res) => {
+// Listen
+const port = process.env.PORT || 8000;
+
+app.use("/", (req, res) => {
   res.send("Hello World!");
 });
-
-const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running at port ${port}`));
-
-export default app;
