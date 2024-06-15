acf2d72ff8ad4746c50ad0e9e22d640a6ebf36c4
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2024-06-15 13:45:39 +0530

    Update index.jsx

--- client/context/index.jsx
+++ client/context/index.jsx
@@ -4,52 +4,46 @@ import axios from "axios";
 
 const UserContext = createContext();
 
-const UserProvider = ({ children }) => { // Corrected syntax: children prop should be lowercase
-    // state when user log in then we update the state
+const UserProvider = ({ children }) => {
     const [state, setState] = useState({
         user: {},
         token: "",
     });
 
     useEffect(() => {
-        setState(JSON.parse(window.localStorage.getItem('auth')));
+        const storedAuth = JSON.parse(window.localStorage.getItem('auth'));
+        if (storedAuth) {
+            setState(storedAuth);
+        }
     }, []);
 
     const router = useRouter();
 
     // Adding token in the config
     const token = state && state.token ? state.token : "";
-    axios.defaults.baseURL = process.env.NEXT_PUBLIC_API;
+    axios.defaults.baseURL = 'https://connxserver.vercel.app/api';
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
     // Adding axios interceptor to handle response errors
     axios.interceptors.response.use(
         function (response) {
-            // If everything is fine, return the response
             return response;
         },
         function (error) {
-            // Handle errors here
             if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
                 console.log("Response status:", error.response.status);
                 console.log("Response data:", error.response.data);
                 console.log("Response headers:", error.response.headers);
                 if (error.response.status === 401) {
-                    // Handle unauthorized access (e.g., token expired)
-                    setState(null);
+                    setState({ user: {}, token: "" });
                     window.localStorage.removeItem("auth");
                     router.push('/login');
                 }
             } else if (error.request) {
-                // The request was made but no response was received
                 console.log("No response received:", error.request);
             } else {
-                // Something happened in setting up the request that triggered an Error
                 console.log("Error:", error.message);
             }
-            // Return the error
             return Promise.reject(error);
         }
     );
