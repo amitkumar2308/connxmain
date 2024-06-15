268cb74449a3964f25b73043aada81c40954b074
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2024-06-15 08:16:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -64,6 +64,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/login', (req, res) => {
+  res.send('Hello Login!');
+});
+
 // Listen
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running at port ${port}`));
