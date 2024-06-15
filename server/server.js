f147c4d14b497ce6bfc77ee3ac52b94d30c3c5fa
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2024-06-15 14:43:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,60 +1,48 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
+import { readdirSync } from 'fs/promises'; // Use fs/promises for modern syntax
 import morgan from 'morgan';
-import { readdirSync } from 'fs';
 import dotenv from 'dotenv';
+import allowCors from './allowCors.js'; // Adjust path as necessary
 
 dotenv.config();
 
 const app = express();
 
 // Database connection
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("Database connected"))
-.catch((err) => console.log("DB connection error =>", err));
+mongoose.connect(process.env.DATABASE)
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.log("DB connection error =>", err));
 
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-// CORS Configuration
-const allowedOrigins = ["https://connx.vercel.app"]; // Specify your allowed origins here
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow headers
-};
-app.use(cors(corsOptions));
+// CORS middleware
+app.use(allowCors);
 
 // Autoload Routes
-const routesPath = './routes';
-readdirSync(routesPath).forEach(async (file) => {
-    if (file.endsWith('.js')) {
-        const route = await import(`${routesPath}/${file}`);
-        app.use('/api', route.default);
+(async () => {
+    const routesPath = './routes';
+    try {
+        const files = await readdirSync(routesPath);
+        for (const file of files) {
+            if (file.endsWith('.js')) {
+                const route = await import(`${routesPath}/${file}`);
+                app.use('/api', route.default);
+            }
+        }
+    } catch (err) {
+        console.error(`Error loading routes: ${err.message}`);
     }
-});
+})();
 
 // Error handling middleware
 app.use((err, req, res, next) => {
     if (err.name === "UnauthorizedError") {
         console.log(err); // Log the error object
         return res.status(401).json({ error: "Unauthorized" });
-    }
-    if (err instanceof Error && err.message === 'Not allowed by CORS') {
-        return res.status(403).json({ error: 'CORS error' });
     }
     next();
 });
