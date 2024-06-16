2cf2889bf3d6fd30a6eb7ff709c6746bccf5ca55
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2024-06-16 08:22:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,33 +5,32 @@ import { readdirSync } from 'fs';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
+
 dotenv.config();
 
 const app = express();
 
 // Database connection
-mongoose.connect(process.env.DATABASE)
-    .then(() => console.log("Database connected"))
-    .catch((err) => console.log("DB connection error =>", err));
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Database connected"))
+.catch((err) => console.log("DB connection error =>", err));
 
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ["https://connx.vercel.app"],
+    origin: ["http://connx.vercel.app"],
 }));
 
 // Autoload Routes
 (async () => {
-    const routesPath = './routes';
-    try {
-        for (const file of readdirSync(routesPath)) {
-            const route = await import(`${routesPath}/${file}`);
-            app.use('/api', route.default);
-        }
-    } catch (err) {
-        console.error(`Error loading routes: ${err.message}`);
+    for (const file of readdirSync('./routes')) {
+        const route = await import(`./routes/${file}`);
+        app.use('/api', route.default);
     }
 })();
 
