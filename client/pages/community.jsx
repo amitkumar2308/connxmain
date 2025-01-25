8ea6296f835581acbe679359e098928bfea39608
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2025-01-25 15:11:00 +0530

    Update community.jsx

--- client/pages/community.jsx
+++ client/pages/community.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Mail, WhatsApp } from '@mui/icons-material';
+import { WhatsApp } from '@mui/icons-material';
 import Image from 'next/image';
 
 const Community = () => {
-  const handleMailClick = () => {
-    window.location.href = 'mailto:amitkgupta2308@gmail.com';
-  };
-
   const handleJoinClick = () => {
     // Add functionality for joining the community
     window.open('https://chat.whatsapp.com/LnH7GvcK3sV541KWMtr5RY', '_blank'); // Open WhatsApp group link in a new tab
@@ -14,22 +10,7 @@ const Community = () => {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-8"> {/* First Box: Co-founder */}
-        <div className="md:flex">
-          <div className="md:flex-shrink-0">
-            <Image
-              className="h-48 w-full object-cover md:w-48 rounded-md"
-              src="/connxprofile.png"
-              alt="Community Image"
-              width={900} // Specify the width of the image
-              height={900} // Specify the height of the image
-            />
-          </div>
-          
-          </div>
-        </div>
-      </div>
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-8"> {/* Second Box: Joining Community */}
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-8"> {/* Box: Joining Community */}
         <div className="md:flex">
           <div className="md:flex-shrink-0">
             <Image
@@ -55,7 +36,7 @@ const Community = () => {
                 className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-black hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
                 onClick={handleJoinClick}
               >
-                <WhatsApp  className="h-5 w-5 mr-2"/>
+                <WhatsApp className="h-5 w-5 mr-2" />
                 Join Community
               </button>
             </div>
